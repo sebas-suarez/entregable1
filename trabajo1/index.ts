f0ce65c774e67d31b4b1d19aaed7aa6d3ac242f8
Sebastian Suàrez <57646872+sebas-suarez@users.noreply.github.com>
2024-09-25 18:55:11 -0300

    Add files via upload

--- trabajo1/index.ts
+++ trabajo1/index.ts
@@ -7,6 +7,7 @@ function calcularPuntaje(sabor:number, presentacion:number, dificultad:number):n
 
 function ganador(){
     let cantidad:number=rs.questionInt('Cuantos participantes son? ');
+
     for(let i=1;i<=cantidad;i++){
         let sabor:number=rs.questionInt('Puntua el sabor del 1 al 5 del cuncursante '+' '+ i+' ');
         let presentacion:number=rs.questionInt('Puntua la presentacion del 1 al 5 del concursante '+' '+ i+' ');
@@ -17,15 +18,19 @@ function ganador(){
 
         if(total>=totalMax){
             totalMax=total;
+            ganadorDelConcurso=i;
         }
 
     };
     
-    console.log('el maximo puntaje fue ' + totalMax);
+    console.log('el maximo puntaje fue ' + totalMax+' del concursante Nro: '+ ganadorDelConcurso);
     
 }
 
+
+let ganadorDelConcurso:number=0;
 let totalMax:number=0;
+
 ganador();
 
 
