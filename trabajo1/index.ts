3bc6396b3a1342cf196b24c95a57c7296cb8b73b
Sebastian Suàrez <57646872+sebas-suarez@users.noreply.github.com>
2024-09-25 18:00:34 -0300

    Add files via upload

---
+++ trabajo1/index.ts
@@ -0,0 +1,33 @@
+import * as rs from 'readline-sync';
+
+function calcularPuntaje(sabor:number, presentacion:number, dificultad:number):number{
+    //let total:number = sabor+presentacion+dificultad;
+    return sabor+presentacion+dificultad;
+}
+
+function ganador(){
+    let cantidad:number=rs.questionInt('Cuantos participantes son? ');
+    for(let i=1;i<=cantidad;i++){
+        let sabor:number=rs.questionInt('Puntua el sabor del 1 al 5 del cuncursante '+' '+ i+' ');
+        let presentacion:number=rs.questionInt('Puntua la presentacion del 1 al 5 del concursante '+' '+ i+' ');
+        let dificultad:number=rs.questionInt('Puntua la dificultad de la receta del 1 al 5 del concursante '+' '+ i +' ');
+
+        let total=calcularPuntaje(sabor,presentacion,dificultad);
+        
+
+        if(total>=totalMax){
+            totalMax=total;
+        }
+
+    };
+    
+    console.log('el maximo puntaje fue ' + totalMax);
+    
+}
+
+let totalMax:number=0;
+ganador();
+
+
+
+
